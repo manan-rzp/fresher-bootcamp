57d4b21d057ac02e53d669959b6b78fa0e864bd8
manan-rzp <manan.agarwal@razorpay.com>
2021-07-20 18:19:46 +0530

    bootcamp

--- q1.go
+++ q1.go
@@ -1,13 +1,16 @@
 package main
+
 import (
 	"fmt"
 	"math/rand"
 )
+
 type Matrix struct {
 	row int
 	col int
 	matrix [][]int
 }
+
 func new_Matrix(r int, c int) Matrix{
 	m := Matrix{
 		row: r,
@@ -23,19 +26,38 @@ func new_Matrix(r int, c int) Matrix{
 	}
 	return m
 }
+
+
+func (m Matrix) print() bool {
+	fmt.Println("[" )
+
+	for i := 0; i < m.row; i++ {
+		fmt.Printf("%s", "[")
+		for j := 0; j < m.col; j++ {
+			fmt.Printf("%d,",m.matrix[i][j])
+		}
+		fmt.Println("],")
+	}
+	fmt.Println("]")
+	return true
+}
+
 func (m Matrix) get_row() int {
 	return m.row
 }
+
 func (m Matrix) get_col() int {
 	return m.col
 }
-func (m Matrix) set_value(i int , j int, val int) bool {
+
+func (m Matrix) set_value(i, j, v int) bool {
 	if i >= m.row && j >= m.col {
 		return false
 	}
-	m.matrix[i][j] = val
+	m.matrix[i][j] = v
 	return true
 }
+
 func (m Matrix) add(M Matrix) Matrix {
 	for i := 0; i < m.row; i++ {
 		for j := 0; j < m.col; j++ {
@@ -44,25 +66,16 @@ func (m Matrix) add(M Matrix) Matrix {
 	}
 	return m
 }
-func (m Matrix) print() bool {
-	fmt.Printf("{" )
-	for i := 0; i < m.row; i++ {
-		fmt.Printf("%s", "{")
-		for j := 0; j < m.col; j++ {
-			fmt.Printf("%d,",m.matrix[i][j])
-		}
-		fmt.Printf("},")
-	}
-	fmt.Printf("}")
-	return true
-}
+
 
 func main(){
 	m1 := new_Matrix(4, 4)
 	m2 := new_Matrix(4, 4)
 
 	fmt.Println(m1.get_row())
+
 	fmt.Println(m1.get_col())
+
 	m3 := m1.add(m2)
 	m3.print()
 }
